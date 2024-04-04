f02339d0e16b319b9f540333b3c9c122ede2dc8d
Priyanshu <mehtapriyanshu408@gmail.com>
2024-04-04 19:33:01 +0530

    ProcessInfo class added to fetch the details about the given process

--- src/Algorithms/ProcessInfo.java
+++ src/Algorithms/ProcessInfo.java
@@ -1,2 +1,20 @@
-package Algorithms;public class ProcessInfo {
+package Algorithms;
+
+public class ProcessInfo {
+    public int[] arrivalTime;
+    public int[] burstTime;
+    public int[] waitingTime;
+    public int[] turnAroundTime;
+
+    public double avgWaitTime;
+    public double avgTAT;
+    public double throughput;
+
+    public ProcessInfo(int[] arrivalTime, int[] burstTime) {
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+
+        waitingTime = new int[arrivalTime.length];
+        turnAroundTime = new int[arrivalTime.length];
+    }
 }
